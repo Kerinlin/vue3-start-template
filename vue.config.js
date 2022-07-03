d32379a32c8fe12229b7a2997e19136ea9184f36
Kerinlin <1058789909@qq.com>
2022-07-03 18:30:47 +0800

    Init template

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
+const UnoCSS = require('unocss/webpack').default;
+const { presetUno, presetAttributify } = require('unocss');
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      UnoCSS({
+        presets: [presetUno(), presetAttributify()],
+        rules: [
+          // custom-margin
+          [/^ml-(\d+)$/, ([, d]) => ({ 'margin-left': `${d}px` })],
+          [/^mr-(\d+)$/, ([, d]) => ({ 'margin-right': `${d}px` })],
+          [/^mt-(\d+)$/, ([, d]) => ({ 'margin-top': `${d}px` })],
+          [/^mb-(\d+)$/, ([, d]) => ({ 'margin-bottom': `${d}px` })],
+          [/^m-(\d+)$/, match => ({ margin: `${match[1]}px` })],
+          // custom-padding
+          [/^pb-(\d+)$/, ([, d]) => ({ 'padding-bottom': `${d}px` })],
+          [/^pt-(\d+)$/, ([, d]) => ({ 'padding-top': `${d}px` })],
+          [/^pl-(\d+)$/, ([, d]) => ({ 'padding-left': `${d}px` })],
+          [/^pr-(\d+)$/, ([, d]) => ({ 'padding-right': `${d}px` })],
+          [/^p-(\d+)$/, match => ({ padding: `${match[1]}px` })],
+        ],
+        shortcuts: {
+          'init-btn': 'border-none cursor-pointer outline-none',
+          'space-between': 'flex flex-row justify-between items-center',
+          'flex-start': 'flex flex-row justify-start items-center',
+        },
+      }),
+    ],
+  },
 });
